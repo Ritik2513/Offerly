70b09ff93b6d0c30eac508595ff2588bd442e7bc
Ritik <ritikgupta2513@gmail.com>
2026-06-04 09:28:19 +0530

    deployment setup

--- server/app.js
+++ server/app.js
@@ -10,11 +10,17 @@ import router from "./routes/index.js";
 
 const app = express();
 
+const allowedOrigins = ["http://localhost:5173", process.env.CLIENT_URL];
+
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(helmet());
